fa5d8b34024b3d6a9d27d78441cb87e8145aed2e
jvs-- <jessica.schulze@oist.jp>
2013-07-17 13:49:11 +0900

    wth

--- pieman.py
+++ pieman.py
@@ -6,32 +6,41 @@ from sklearn import cluster
 from sklearn.decomposition import FastICA
 
 
-vox_data = scipy.io.loadmat('pieman_meanLis.mat')
+vox_data = scipy.io.loadmat('reverse_meanLis.mat')
 meanVox = vox_data["meanVox"] #unpacking clutter
 
 #Let's take a look at what we are working with
 v = len(meanVox)
 t = len(meanVox[1])
-print "Time steps ", t          # 38326
-print "Number of voxels ", v    # 259
+print "Time steps ", t          # 35593
+print "Number of voxels ", v    # 260
 
-#We got 38326 voxels a 259 measurements
+#We got 35593 voxels a 260 measurements
 #Assuming the number of time steps is the same for all entries
 
 #Let's take a look at another data set
 #Broadman indices for the voxels 
 brod_data = scipy.io.loadmat('brodmann_area.mat')
-indices = data["brodmannIdx"][0] #unpacking clutter
+indices = brod_data["brodmannIdx"][0] #unpacking clutter
+
 max_idx = len(indices)
 print "Number of indexed voxels", max_idx
 print "Up to Brodman area ", indices.max() # 47
 
-#Not good. Number of indeces (35593) does not match with number of voxels (38326) 
-
 #Let's see if naively clustering voxels using kmeans into 47 classes
 #will show any overlap with the actualy recorded indices we got from brodmanIdx
-#Doing this for all 30k voxels would take for ever. let's first look at a couple of hundred
-mv = meanVox[:100]
+#Working with all 30k voxels will take seceral min.
+#TODO: Considere what a good fraction of the data is to develop on
+mv = meanVox
 k_means = cluster.KMeans(n_clusters=47) 
 k_means.fit(mv)
-print k_means.labels_[::10]
+#Suppose if we use reverse_meansList we will also have to reverse our indices
+rev_indices = indices[::-1] 
+labels = k_means.labels_
+
+#There is no obvious overlap of clusteres for just the first 100 voxels
+#TODO: Let's try checking whether rev_indices and the classes kmeans found have 
+#any correlation 
+
+
+
